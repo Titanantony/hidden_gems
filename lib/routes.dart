91932f70ca177f6ef89ffb51b3c38dd2ce9d39fb
Titanantony <antonytitan96@gmail.com>
2024-06-08 17:13:09 +0300

    Chore: Adjust routes for the first page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:hidden_gems/ui/screens/first_page.dart';
 import 'package:hidden_gems/ui/screens/home_page.dart';
 import 'package:hidden_gems/ui/screens/sign_in_screen.dart';
 import 'package:hidden_gems/ui/screens/sign_up_screen.dart';
 
 class Routes {
   static var routes = [
+    GetPage(name: "/First-Page", page: () => const FirstPage()),
     GetPage(name: "/Sign-In", page: () => const LoginScreen()),
     GetPage(name: "/Sign-Up", page: () => const SignUpScreen()),
     GetPage(name: "/Home-Page", page: () => const HomePage()),
--- lib/ui/utils/sigup_funtion.dart
+++ lib/ui/utils/sigup_funtion.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+// import 'package:get/get_core/src/get_main.dart';
 
 Future<void> firebaseSignup(String emailController, String passwordController,
     BuildContext context) async {
