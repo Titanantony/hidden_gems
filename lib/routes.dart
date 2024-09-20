6f097218355471eeea33193ab79b63b4e0890a0e
Titan antony <antonytitan96@gmail.com>
2024-09-20 21:18:08 +0300

    Chore: update routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:hidden_gems/ui/screens/homepage/explore_page.dart';
 import 'package:hidden_gems/ui/screens/first_page.dart';
 import 'package:hidden_gems/ui/screens/homepage/homePage.dart';
 import 'package:hidden_gems/ui/screens/sign_in_screen.dart';
@@ -10,5 +11,8 @@ class Routes {
     GetPage(name: "/Sign-In", page: () => const LoginScreen()),
     GetPage(name: "/Sign-Up", page: () => const SignUpScreen()),
     GetPage(name: "/Home-Page", page: () => const HomePage()),
+    GetPage(
+        name: "/Explore-page",
+        page: () => ExplorePage(factory: FirebaseExplorePageFactory())),
   ];
 }
